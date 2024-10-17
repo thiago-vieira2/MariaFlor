d1546fd4a854f767ba27ab709293cb9fa1ca30d9
beferreira <ra43020907829@acaonsfatima.org.br>
2024-10-17 15:08:00 -0300

    oi

--- reacttcc/src/pages/contato/contato.jsx
+++ reacttcc/src/pages/contato/contato.jsx
@@ -2,17 +2,39 @@ import './contato.scss';
 import Cabecalho from '../../components/cabecalho/cabecalho.jsx';
 import Rodape from '../../components/rodape/rodape.jsx';
 
+
+
 function Contato(){
 
+  const dragEvents = {
+    onDragEnter: (e) => {
+      e.preventDefault();
+      console.log('onDragEnter')
+    },
+
+    onDragLeave: (e) => {
+      e.preventDefault();
+      (console.log('onDragLeave'));
+    },
+
+    onDragOver: (e) => {
+      e.preventDefault();
+      (console.log('onDragOver'));
+    },
+    onDrop: (e) => {
+      e.preventDefault();
+      (console.log('onDrop'));
+    },
+  }
   
   const handleDownload = () => {
     // URL do PDF que você deseja baixar
-    const pdfUrl = '/caminho/para/seu/arquivo.pdf';
+    const pdfUrl = './documents/Política de Privacidade Front Company.pdf';
     
     // Criando um link temporário para download
     const link = document.createElement('a');
     link.href = pdfUrl;
-    link.setAttribute('download', 'seu-arquivo.pdf'); // Nome do arquivo ao ser baixado
+    link.setAttribute('download', 'Política de Privacidade Front Company.pdf'); // Nome do arquivo ao ser baixado
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -33,22 +55,24 @@ function Contato(){
           <div className='campos'>
             <input placeholder='Nome' type="text" />
             <input placeholder='E-Mail' type="text" />
-            <input placeholder='Tel. ex.: (XX) X XXXX-XXXX' type="text" />
+            <input required pattern="[0-9]{2} [0-9]{5}-[0-9]{4}" placeholder="Tel. ex.: 11 99999-9999" type="tel"/>
             <input placeholder='CEP' type="text" />
           </div>
 
-          <div className='imagem'>
+          <div className='imagem' {...dragEvents}>
+            
+            <p>Arraste seu anexo para este campo</p>
 
           </div>
         </div>
         <div className='mensagem'>
-          <input placeholder='Insira aqui sua mensagem' type="text"/>
+          <textarea placeholder='Insira aqui sua mensagem' name="" id=""></textarea>
         </div>
         <div className='politica'>
-          <div className='escrita'>
-             <p>Declaro que li e estou de acordo com a </p> <a href="" onClick={handleDownload}> Política de Privacidade </a> <p>da Maria Flor.</p>
-          </div>
-          <input type="checkbox" />
+            <div className='escrita'>
+              <p>Declaro que li e estou de acordo com a </p> <a href="" onClick={handleDownload}> Política de Privacidade </a> <p>da Maria Flor.</p>
+            </div>
+            <input type="checkbox" />
           </div>
 
         <button>Enviar</button>
