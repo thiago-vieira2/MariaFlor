4d20859990ea08deaffbbdca668e3103d82779be
beferreira <ra43020907829@acaonsfatima.org.br>
2024-10-21 15:30:35 -0300

    card

--- reacttcc/src/components/card-produto/cardProduto.jsx
+++ reacttcc/src/components/card-produto/cardProduto.jsx
@@ -2,8 +2,14 @@ import './cardProduto.scss';
 
 function CardProduto() {
   return (
-    <div className="card-produto">
-
+    <div className="card">
+      <span className="card__number">#62</span>
+      <div className="card__image">
+        <img src="./images/delicia_chocolate.png" alt="Delicia de Chocolate" />
+      </div>
+      <div className="card__content">
+        <h3 className="card__title">Delicia de Chocolate</h3>
+      </div>
     </div>
   );
 }
--- reacttcc/src/pages/inicio/inicio.jsx
+++ reacttcc/src/pages/inicio/inicio.jsx
@@ -1,6 +1,7 @@
 import './inicio.scss';
 import Cabecalho from '../../components/cabecalho/cabecalho.jsx';
 import Rodape from '../../components/rodape/rodape.jsx';
+import CardProduto from '../../components/card-produto/cardProduto.jsx';
 
 
 function Inicio() {
@@ -14,6 +15,14 @@ function Inicio() {
       <div className='faixa'>
         <a href="/salgados"> <img src="./images/salgado.jpg" alt="" /></a>
         <a href="/doces"> <img src="./images/doce.avif" alt="" /></a>
+      </div>
+
+      <div className='cards'>
+        <div className='add-card'>
+          <img src="./images/add.png" alt="" />
+          <h2>Adicionar Produto</h2>
+        </div>
+        <CardProduto/>
       </div>
     </div>
 
