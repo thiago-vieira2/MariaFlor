f922a655b608e50270aa9e4d8ac5e7edf36300e3
beferreira <ra43020907829@acaonsfatima.org.br>
2024-10-23 15:30:04 -0300

    23-10

--- reacttcc/src/pages/contato/contato.jsx
+++ reacttcc/src/pages/contato/contato.jsx
@@ -1,93 +1,111 @@
-import './contato.scss';
+import './contato.scss'; 
 import Cabecalho from '../../components/cabecalho/cabecalho.jsx';
 import Rodape from '../../components/rodape/rodape.jsx';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-function Contato(){
+function Contato() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [cep, setCep] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [politica, setPolitica] = useState(false);
+
+  const inputFileRef = useRef(null);
+  const pictureImageRef = useRef(null);
+  const pictureImageTxt = "Buscar imagem no dispositivo";
+
+  useEffect(() => {
+    pictureImageRef.current.innerHTML = pictureImageTxt;
+
+    const handleFileChange = (e) => {
+      const file = e.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          const img = document.createElement("img");
+          img.src = event.target.result;
+          img.classList.add("picture__img");
+
+          pictureImageRef.current.innerHTML = "";
+          pictureImageRef.current.appendChild(img);
+        };
+        reader.readAsDataURL(file);
+      } else {
+        pictureImageRef.current.innerHTML = pictureImageTxt;
+      }
+    };
+
+    const inputFile = inputFileRef.current;
+    inputFile.addEventListener("change", handleFileChange);
+
+    return () => {
+      inputFile.removeEventListener("change", handleFileChange);
+    };
+  }, []);
 
   const dragEvents = {
-    onDragEnter: (e) => {
-      e.preventDefault();
-      console.log('onDragEnter')
-    },
+    onDragEnter: (e) => { e.preventDefault(); console.log('onDragEnter'); },
+    onDragLeave: (e) => { e.preventDefault(); console.log('onDragLeave'); },
+    onDragOver: (e) => { e.preventDefault(); console.log('onDragOver'); },
+    onDrop: (e) => { e.preventDefault(); console.log('onDrop'); },
+  };
 
-    onDragLeave: (e) => {
-      e.preventDefault();
-      (console.log('onDragLeave'));
-    },
-
-    onDragOver: (e) => {
-      e.preventDefault();
-      (console.log('onDragOver'));
-    },
-    onDrop: (e) => {
-      e.preventDefault();
-      (console.log('onDrop'));
-    },
-  }
-  
   const handleDownload = () => {
-    // URL do PDF que você deseja baixar
     const pdfUrl = './documents/Política de Privacidade Front Company.pdf';
-    
-    // Criando um link temporário para download
     const link = document.createElement('a');
     link.href = pdfUrl;
-    link.setAttribute('download', 'Política de Privacidade Front Company.pdf'); // Nome do arquivo ao ser baixado
+    link.setAttribute('download', 'Política de Privacidade Front Company.pdf');
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-  }
+  };
 
-  const [nome,setNome] = useState('')
-  const [email,setEmail] = useState('')
-  const [telefone,setTelefone] = useState('')
-  const [cep,setCep] = useState('')
-  const [descricao,setDescricao] = useState('')
-
-  const [politica,setPolitica] = useState(false)
-
-    return (
+  return (
     <div className="pagina-contato">
       <header className="cabecalho">
-        <Cabecalho/>
+        <Cabecalho />
       </header>
 
-        <img className='cappuccino' src="./images/cappuccino.jpg" alt=""  />
+      <img className='cappuccino' src="./images/cappuccino.jpg" alt="" />
 
-        <h1>Deixe sua mensagem</h1>
+      <h1>Deixe sua mensagem</h1>
 
       <div className='interativo'>
         <div className='cima'>
           <div className='campos'>
-            <input placeholder='Nome' type="text" value={nome} onChange={e => setNome(e.target.value)}/>
-            <input placeholder='E-Mail' type="text" value={email} onChange={e => setEmail(e.target.value)}/>
-            <input placeholder='Tel. ex.: (XX) X XXXX-XXXX' type="text" value={telefone} onChange={e => setTelefone(e.target.value)}/>
-            <input placeholder='CEP' type="text" value={cep} onChange={e => setCep(e.target.value)}/>
+            <input placeholder='Nome' type="text" value={nome} onChange={e => setNome(e.target.value)} />
+            <input placeholder='E-Mail' type="text" value={email} onChange={e => setEmail(e.target.value)} />
+            <input placeholder='Tel. ex.: (XX) X XXXX-XXXX' type="text" value={telefone} onChange={e => setTelefone(e.target.value)} />
+            <input placeholder='CEP' type="text" value={cep} onChange={e => setCep(e.target.value)} />
           </div>
 
           <div className='imagem' {...dragEvents}>
-            
-            <p>Arraste seu anexo para este campo</p>
-
+            <label className="picture" htmlFor="picture__input" tabIndex="0">
+              <span className="picture__image" ref={pictureImageRef}></span>
+            </label>
+            <input type="file" name="picture__input" id="picture__input" ref={inputFileRef} />
           </div>
         </div>
+
         <div className='mensagem'>
-          <textarea placeholder='Insira aqui sua mensagem' type="text" value={descricao} onChange={e => setDescricao(e.target.value)}/>
+          <textarea placeholder='Insira aqui sua mensagem' type="text" value={descricao} onChange={e => setDescricao(e.target.value)} />
         </div>
+
         <div className='politica'>
           <div className='escrita'>
-             <p>Declaro que li e estou de acordo com a </p> <a href="" onClick={handleDownload}> Política de Privacidade </a> <p>da Maria Flor.</p>
+            <p>Declaro que li e estou de acordo com a </p>
+            <a href="" onClick={handleDownload}> Política de Privacidade </a>
+            <p>da Maria Flor.</p>
           </div>
-          <input type="checkbox" />
-          </div>
+          <input type="checkbox" checked={politica} onChange={() => setPolitica(!politica)} />
+        </div>
 
         <button>Enviar</button>
       </div>
 
-      
-        <Rodape/>
-    </div>  
+      <Rodape />
+    </div>
   );
 }
 
