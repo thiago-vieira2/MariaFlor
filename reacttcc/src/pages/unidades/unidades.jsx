0cc64b5ba672b9c6ea96c10e277d7222973893bd
beferreira <ra43020907829@acaonsfatima.org.br>
2024-10-29 01:01:22 -0300

    Falta só o dashboard

--- reacttcc/src/pages/unidades/unidades.jsx
+++ reacttcc/src/pages/unidades/unidades.jsx
@@ -3,7 +3,31 @@ import Cabecalho from '../../components/cabecalho/cabecalho.jsx';
 import Rodape from '../../components/rodape/rodape.jsx';
 import CardUnidade from '../../components/card-unidade/cardUnidade.jsx';
 
+const unidades = [
+  {
+    "img": "./images/doceria.jpg",
+    "endereco": "Av. José Ribeiro Junqueira, 670 - Jardim Colonial, São Paulo - SP, 04821-020",
+    "abre": "09:00",
+    "fecha": "17:30",
+    "url": "https://maps.app.goo.gl/iQHXHL2ioas8Z14WA"
+  },
 
+  {
+    "img": "./images/doceria.jpg",
+    "endereco": "Av. José Ribeiro Junqueira, 670 - Jardim Colonial, São Paulo - SP, 04821-020",
+    "abre": "09:00",
+    "fecha": "17:30",
+    "url": "https://maps.app.goo.gl/iQHXHL2ioas8Z14WA"
+  },
+
+  {
+    "img": "./images/doceria.jpg",
+    "endereco": "Av. José Ribeiro Junqueira, 670 - Jardim Colonial, São Paulo - SP, 04821-020",
+    "abre": "09:00",
+    "fecha": "17:30",
+    "url": "https://maps.app.goo.gl/iQHXHL2ioas8Z14WA"
+  },
+]
 
 function Unidades() {
   return (
@@ -20,10 +44,16 @@ function Unidades() {
                 <p>Adicionar nova unidade</p>
                 <img src="./images/empresa.png" alt="" width={30}/>
             </a>
+        </div>
 
-            <div className='cards'>
-              <CardUnidade/>
-            </div>
+        <div className='cards'>
+        {unidades.map(u => <CardUnidade
+            img={u.img}
+            endereco={u.endereco}
+            abre={u.abre}
+            fecha={u.fecha}
+            url={u.url}
+            />)}
         </div>
     </div>
 
